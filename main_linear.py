6d21974cbdf4955344e474be2241abdc7dcf4a6a
statmlben <bendaimath@gmail.com>
2020-09-15 09:54:33 -0500

    update giveup linear case

--- main_linear.py
+++ main_linear.py
@@ -8,17 +8,18 @@ from keras.callbacks import EarlyStopping
 from keras.optimizers import Adam, SGD
 from tensorflow.compat.v1.train import ProximalGradientDescentOptimizer
 from sklearn.model_selection import train_test_split
+from keras import initializers
+
 # plot for 1d case
 # https://scikit-learn.org/stable/auto_examples/linear_model/plot_ransac.html#sphx-glr-auto-examples-linear-model-plot-ransac-py
-
-
 
 n_samples, input_shape, labels = 5000, 100, 1
 
 noise = 10
 X = np.random.randn(n_samples, input_shape)
-beta_true = np.random.randn(input_shape)
-beta_true[int(input_shape/10):] = 0.
+# beta_true = np.random.randn(input_shape)
+beta_true = np.zeros(input_shape)
+beta_true[:int(input_shape/10)] = np.arange(1., 11, 1.)
 y = X.dot(beta_true) + noise*np.random.randn(n_samples)
 
 # X, y, coef = datasets.make_regression(n_samples=n_samples, n_features=input_shape,
@@ -31,79 +32,91 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random
 
 discriminator = build_discriminator(input_dim=input_shape, 
 									labels=labels, 
-									lam=0.0001)
+									lam=0.001)
 
-discriminator.compile(loss='MSE', optimizer=Adam(lr=0.1))
+discriminator.compile(loss='MSE', optimizer=Adam(lr=0.001))
 # discriminator.load_weights("model_noise.h5")
 
 
-es_learn = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=100, restore_best_weights=True)
+es_learn = EarlyStopping(monitor='val_loss', mode='min', verbose=1, 
+						patience=1000, restore_best_weights=False)
 
 # fit discriminator
 print('###'*20)
 print('###'*5+' '*6+'Train for learner'+' '*5+'###'*5)
 print('###'*20)
 
-learn_tmp = discriminator.fit(x=X_train, y=y_train, callbacks=[es_learn], epochs=1000, 
-									batch_size=128, validation_split=.2)
+# learn_tmp = discriminator.fit(x=X_train, y=y_train, callbacks=[es_learn], epochs=1, 
+# 									batch_size=128, validation_split=.2)
 
+discriminator.set_weights(beta_true.reshape(1, input_shape, 1)) 
 train_loss_base = discriminator.evaluate(X_train, y_train)
 test_loss_base = discriminator.evaluate(X_test, y_test)
 
 ## define framework
-# lam_range = [.2]
-lam_range = np.arange(.2, 10.0, .2)
-lam_range = lam_range.astype('float32')
+# lam_range = [.5]
+lam_range = np.arange(200., 260., 10.)
+# lam_range = lam_range.astype('float32')
 R_square_train_lst, R_square_test_lst = [], []
-
+delta_lst = []
 for lam in lam_range:
-	PSGD = ProximalGradientDescentOptimizer(learning_rate = 0.01,
-									l1_regularization_strength = lam)
+	loss_seed, delta_seed, train_loss_seed, test_loss_seed = [], [], [], []
+	for l in range(1):
+		# PSGD = ProximalGradientDescentOptimizer(learning_rate = 0.001,
+		# 								l1_regularization_strength = lam)
+		if l == 0:
+			initializer_tmp = initializers.Ones()
+		else:
+			initializer_tmp = initializers.Zeros()
 
-	detector = build_detector(input_dim=input_shape,
-								regularizer=tf.keras.regularizers.L1(0.), 
-								type_='noise')
+		detector = build_detector(input_dim=input_shape,
+									initializer=initializer_tmp,
+									regularizer=tf.keras.regularizers.L1(lam), 
+									type_='noise')
 
-	shiing = LocalGAN(input_shape=input_shape, 
-						labels=labels,
-						discriminator=discriminator, 
-						detector=detector,
-						optimizer=PSGD,
-						# optimizer=SGD(lr=0.001),
-						task='regression')
+		shiing = LocalGAN(input_shape=input_shape,
+							labels=labels,
+							discriminator=discriminator, 
+							detector=detector,
+							# optimizer=PSGD,
+							optimizer=Adam(lr=0.001),
+							task='regression')
 
-	es_detect = EarlyStopping(monitor='loss', mode='min', min_delta=.0001, verbose=1, patience=1000, restore_best_weights=True)
+		es_detect = EarlyStopping(monitor='loss', mode='min', min_delta=.0001, 
+								verbose=1, patience=100, restore_best_weights=False)
+		
+		# lw = 2
+		# y_pred = shiing.discriminator.predict(X_train)
+		# plt.scatter(X_train, y_pred, color='yellowgreen', marker='.')
+		# plt.scatter(X_train, y_train, color='red', marker='.')
+		# # plt.plot(line_X, line_y, color='navy', linewidth=lw, label='Linear regressor')
+		# # plt.legend(loc='lower right')
+		# # plt.xlabel("Input")
+		# # plt.ylabel("Response")
+		# plt.show()
 
-	# shiing.discriminator.save_weights("model_01.h5")
-	# shiing.discriminator.load_weights("model_test.h5")
-	# shiing.discriminator.load_weights("model_01.h5")
-	# shiing.discriminator.load_weights("model_noise.h5")
+		print('###'*20)
+		print('#'*16+' '*5+'Train for detector'+' '*5+'#'*16)
+		print('###'*20)
 
-	# lw = 2
-	# y_pred = shiing.discriminator.predict(X_train)
-	# plt.scatter(X_train, y_pred, color='yellowgreen', marker='.')
-	# plt.scatter(X_train, y_train, color='red', marker='.')
-	# # plt.plot(line_X, line_y, color='navy', linewidth=lw, label='Linear regressor')
-	# # plt.legend(loc='lower right')
-	# # plt.xlabel("Input")
-	# # plt.ylabel("Response")
-	# plt.show()
+		detect_tmp = shiing.combined.fit(x=X_train, y=y_train, callbacks=[es_detect], 
+										epochs=10000, batch_size=128)
+		loss_seed.append(detect_tmp.history['loss'][-1])
+		## show the results
+		X_train_noise = shiing.detector.predict(X_train)
+		X_test_noise = shiing.detector.predict(X_test)
 
+		delta_tmp = X_train_noise[0] - X_train[0]
+		train_loss_tmp = shiing.discriminator.evaluate(X_train_noise, y_train)
+		test_loss_tmp = shiing.discriminator.evaluate(X_test_noise, y_test)
 
-	print('###'*20)
-	print('#'*16+' '*5+'Train for detector'+' '*5+'#'*16)
-	print('###'*20)
+		delta_seed.append(delta_tmp)
+		train_loss_seed.append(train_loss_tmp), 
+		test_loss_seed.append(test_loss_tmp)
 
-	detect_tmp = shiing.combined.fit(x=X_train, y=y_train, callbacks=[es_detect], 
-									epochs=10000, batch_size=128)
-	# validation_split=.2
-
-	## show the results
-	X_train_noise = shiing.detector.predict(X_train)
-	X_test_noise = shiing.detector.predict(X_test)
-
-	train_loss = shiing.discriminator.evaluate(X_train_noise, y_train)
-	test_loss = shiing.discriminator.evaluate(X_test_noise, y_test)
+	delta_tmp = delta_seed[np.argmin(loss_seed)]
+	train_loss = train_loss_seed[np.argmin(loss_seed)]
+	test_loss = test_loss_seed[np.argmin(loss_seed)]
 
 	print('Baseline: train_loss: %.3f; test_loss: %.3f' %(train_loss_base, test_loss_base))
 	# print('Baseline: train_acc: %.3f; test_acc: %.3f' %(train_acc_base, test_acc_base))
@@ -112,25 +125,26 @@ for lam in lam_range:
 
 	shiing.R_square_train = 1. - train_loss_base / train_loss
 	shiing.R_sqaure_test = 1. - test_loss_base / test_loss
+	
 	print('lam: %.3f; R_square_train: %.3f; R_sqaure_test: %.3f' 
 		%(lam, shiing.R_square_train, shiing.R_sqaure_test))
-	print('true parameter: %s' %beta_true)
-	print('delta: %s' %(X_train_noise[0] - X_train[0]))
+	print('true parameter: %s' %beta_true[:10])
+	print('delta: %s' %delta_tmp[:10])
 	R_square_train_lst.append(shiing.R_square_train)
 	R_square_test_lst.append(shiing.R_sqaure_test)
+	delta_lst.append(delta_tmp)
 
-# print(R_square_train_lst)
-# print(R_square_test_lst)
+print(R_square_train_lst)
+print(R_square_test_lst)
+delta_lst = np.abs(delta_lst)
+import pandas as pd
+import seaborn as sns
+sns.set()
+sns.heatmap(delta_lst)
+plt.show()
 
-# R_square_train_lst = [0.795597143873443, 0.7901153606659239, 0.7846094000733868, 0.7789560364211161, 0.7714482547160242, 0.7629838740349997, 0.7536717177839927, 0.7426291531312593, 0.7302834533541153, 0.7175523748776889, 0.7041529203703243, 0.68509710793004, 0.6701807062595773, 0.653334139669022, 0.6381600634320178, 0.6180961624126166, 0.6030289722959752, 0.553438154618743, 0.31199087160303973, 0.29564680161896484, 0.2836821038485833, 0.2744524557911976, 0.2670626702429504, 0.2500481793877031, 0.2408908893396844, 0.23602767358099952, 0.20995864866136282, 0.18273674568854936, 0.17086601856155814, 0.17766644452546188, 0.1503873404812066, 0.15758883063166018, 0.149453429595371, 0.1448757531079028, 0.0, 0.0013313429568163615, 0.0, 0.0, 0.0, -2.187558242749965e-05, -0.0002391828792451811, 0.0, -0.00024613574386278536, 0.0, 0.0]
-# R_square_test_lst = [0.7620997058251329, 0.7557842104067081, 0.7494478321334521, 0.7429492546116188, 0.7343313413981409, 0.7246321646957352, 0.713982756618035, 0.7013846159078267, 0.6873394423303818, 0.6729014618022003, 0.6577573366330365, 0.6363153497586312, 0.6196114241722354, 0.6008342892915457, 0.5840041412097283, 0.5618745346470839, 0.545351903052025, 0.49157873158821785, 0.24534306568425002, 0.229766317018686, 0.21846560692069905, 0.20980900010261438, 0.2029169382173075, 0.18718423175563137, 0.17879704191082546, 0.17436654290660847, 0.15090640105947273, 0.12696527680311476, 0.1167185120638844, 0.12257370481514773, 0.09934313178587806, 0.10540825991882508, 0.0985604549633633, 0.09473636896275117, 0.0, -0.0014803026897347582, 0.0, 0.0, 0.0, 0.0035592061937771424, 0.0010226265400620216, 0.0, 0.0021385960174660212, 0.0, 0.0]
-
-# import pandas as pd
-# import seaborn as sns
-# R_train = pd.DataFrame({'lam': lam_range, 'R_square': R_square_train_lst, 'Type': ['R_square_train']*45})
-# R_test = pd.DataFrame({'lam': lam_range, 'R_square': R_square_test_lst, 'Type': ['R_square_test']*45})
-# df = pd.concat([R_train, R_test])
-
-# sns.set()
-# sns.lineplot(data=df, x="lam", y="R_square", color='k', markers=True, alpha=.7, style='Type', lw=2.)
-# plt.show()
+R_train = pd.DataFrame({'lam': lam_range, 'R_square': R_square_train_lst, 'Type': ['R_square_train']*len(lam_range)})
+R_test = pd.DataFrame({'lam': lam_range, 'R_square': R_square_test_lst, 'Type': ['R_square_test']*len(lam_range)})
+df = pd.concat([R_train, R_test])
+sns.lineplot(data=df, x="lam", y="R_square", color='k', markers=True, alpha=.7, style='Type', lw=2.)
+plt.show()
--- models/linear_models.py
+++ models/linear_models.py
@@ -20,33 +20,35 @@ from tensorflow.keras.constraints import max_norm
 
 class TabNoise(Layer):
 
-	def __init__(self, output_dim, regularizer=tf.keras.regularizers.L1(1.), **kwargs):
+	def __init__(self, output_dim, regularizer=tf.keras.regularizers.L1(1.), initializer=initializers.Zeros(), **kwargs):
 	   self.output_dim = output_dim
 	   super(TabNoise, self).__init__(**kwargs)
 	   # self.regularizer = regularizers.get(regularizer)
 	   self.regularizer = regularizer
+	   self.initializer = initializer
 	   
 	def build(self, input_shape):
 	   self.D_ = self.add_weight(name='delta', 
 	   								shape=(self.output_dim,),
 	   								regularizer=self.regularizer,
 	   								# constraint=max_norm(max_value=2, axis=0),
-	   								initializer=initializers.RandomNormal(seed=0), 
+	   								# initializer=initializers.RandomNormal(seed=0), 
+	   								initializer=self.initializer,
 	   								trainable=True)
 	   super(TabNoise, self).build(input_shape)
 
 	def call(self, X):
-	   # return K.sum([X, -self.D_])
 	   return X - K.tanh(self.D_)
+	   # return X - K.sigmoid(self.D_) - .5
 
 	def compute_output_shape(self):
 	   return self.output_dim
 
 
-def build_detector(input_dim, regularizer, type_='mask'):
+def build_detector(input_dim, regularizer, initializer, type_='mask'):
 	model = Sequential()
 	model.add(tf.keras.Input(shape=input_dim))
-	model.add(TabNoise(input_dim, regularizer=regularizer))
+	model.add(TabNoise(input_dim, regularizer=regularizer, initializer=initializer))
 	# if type_ == 'mask':
 	# 	model.add(Activation(activations.tanh))
 	# else:
@@ -58,9 +60,10 @@ def build_discriminator(input_dim, lam, labels=1):
 
 	model = Sequential()
 	model.add(Dense(labels, input_dim=input_dim,
+		use_bias=False,
 		kernel_regularizer=tf.keras.regularizers.L1(lam),
 		bias_regularizer=tf.keras.regularizers.L1(lam),
-		kernel_initializer=initializers.glorot_uniform(seed=0)))
+		kernel_initializer=initializers.Zeros()))
 	# model.summary()
 
 	X = Input(shape=input_dim)
